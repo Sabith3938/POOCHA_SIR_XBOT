ce7fae8f685969e85207c9383128fc3dd2b09e01
Sabith3938 <82069764+Sabith3938@users.noreply.github.com>
2023-07-09 11:55:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MyBotzz/RashmikaBot.git /RashmikaBot
+  git clone https://github.com/Sabith3938/POOCHA_SIR_XBOT /RashmikaBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /RashmikaBot
+  git clone $UPSTREAM_REPO /POOCHA_SIR_XBOT
 fi
-cd /RashmikaBot
+cd /POOCHA_SIR_XBOT
 pip3 install -U -r requirements.txt
 echo "Starting JENNIE_DUDE...."
 python3 bot.py
