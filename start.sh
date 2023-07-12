6c2397d942a5ca9aa0588495e4674e4b440ae5bc
Sabith3938 <82069764+Sabith3938@users.noreply.github.com>
2023-07-12 15:38:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sabith3938/POOCHA_SIR_XBOT /POOCHA_SIR_XBOT
+  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot /PiroAutoFilterBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /POOCHA_SIR_XBOT
+  git clone $UPSTREAM_REPO /PiroAutoFilterBot
 fi
-cd /POOCHA_SIR_XBOT
+cd /PiroAutoFilterBot
 pip3 install -U -r requirements.txt
 echo "Starting JENNIE_DUDE...."
 python3 bot.py
